1dda5fdf615a5bfc4acb90ccdd83f3064c0e3647
“ComputerChicken” <gamegamemanage@gmail.com>
2026-07-28 13:38:21 -0600

    added rate limiter

--- server/main.js
+++ server/main.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const fs = require('node:fs/promises');
 const cors = require('cors');
+const { rateLimit } = require('express-rate-limit');
 
 const app = express();
 const port = 21478;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false
+});
+
+app.use(limiter)
 
 app.use(cors());
 
